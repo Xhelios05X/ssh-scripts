488c455d00104d2e59f82cd1dd3781feb5e512e8
BartłomiejGazduslki <heluios05@gmail.com>
2024-06-05 21:16:14 +0200

    like previous

--- sshConnectMonitor.py
+++ sshConnectMonitor.py
@@ -4,6 +4,7 @@ import os
 import time
 import argparse
 
+# help function 
 def Help():
     parser = argparse.ArgumentParser(
         prog = "sshConnectMonitor.py"
@@ -19,9 +20,12 @@ To kill process you have to use 'kill' command
 
 # function uses linux command "lastb" to check ssh logs
 def badConnectionsCounter() -> int:
+
+    # by 'os' library and 'system' method script can run linux commands 
     cmdOutput = os.system("sudo lastb | grep ssh")
     return int(cmdOutput)
 
+# function is checking that ssh logs have a new entry
 def badConnectionsExamine():
     pattern = badConnectionsCounter()
     time.sleep(60)
@@ -29,13 +33,15 @@ def badConnectionsExamine():
     while True:
         entryNumber = badConnectionsCounter()
 
+        # if ssh logs have a new entry,
+        # a warning is printed on the terminal
+        # and new the pattern has a new value
         if pattern < entryNumber:
             print("WARNING: New unsuccessful connection")
             pattern = entryNumber
-
+            
+        # after a checking condition, the script sleeps for 1 minute
         time.sleep(60)
-
-    
 
 # main function of program
 if __name__ == "__main__":
