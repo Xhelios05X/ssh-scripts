9fda2eddd9037a7859643b9ad3cb402b5a538e2b
BartłomiejGazduslki <heluios05@gmail.com>
2024-06-05 17:10:48 +0200

    script sshConnectMonitor.py was added

---
+++ sshConnectMonitor.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+import os
+
+# function uses unixlike command "lastb" to check 
+def badConnectionsCounter() -> int:
+    cmdOutput = os.system("sudo lastb | grep ssh")
+    return int(cmdOutput)
+
+def badConnectionsExamine():
+
+# main function of program
+if __name__ == "__main__":
+    badConnections()
