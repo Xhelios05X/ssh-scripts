4bb6a7128e1fb55738b726d9bb87a61a21be732f
BartłomiejGazduslki <heluios05@gmail.com>
2024-06-06 12:36:16 +0200

    in sftpFilesSent.sh comments and help were added

--- sftpFilesSent.sh
+++ sftpFilesSent.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+# function returns the help of this script
 function Help(){
-    echo "Usage: "
+    echo "sftpFilesSent.sh - script to sending files into host via ssh
+
+usage: ./sftpFilesSent.sh [USERNAME] [IP] [FILES] [DIRECTORY]
+options:
+    -h          help
+
+scipt is created by Xhelios05X
+github.com/Xhelios05X"
     exit 0
 }
 
+# function archives and compress directory
 # $1 - directory to archive
 function archiveDirectory(){
     tar -czvf $1.tar.gz $1
@@ -23,8 +32,10 @@ function main(){
 
     archiveDirectory $3
 
+    # sending files via ssh connection
     scp $3.tar.gz $1@$2:$4
 
+    # removing .tar.gz file from local directory
     rm $3.tar.gz
 }
 
